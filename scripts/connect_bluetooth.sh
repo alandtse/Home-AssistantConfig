78da71415b525598f10f5d03fcb91176b6d61277
Alan Tse <alandtse@gmail.com>
2017-11-24 21:37:17 -0800

    Adding additional tts output

--- scripts/connect_bluetooth.sh
+++ scripts/connect_bluetooth.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
-bluetoothctl << EOF
-trust 84:D6:D0:21:85:21 
-connect 84:D6:D0:21:85:21 
-EOF
-sleep 10
-pactl set-card-profile `pactl list short | grep bluez_card | awk '{print $1}'` a2dp
+test=`pactl list short | grep bluez_sink`
+if [[ -z $test ]]; then
+  /home/homeassistant/.homeassistant/scripts/bluetooth.sh 84:D6:D0:21:85:21
+  sleep 4
+  pactl set-card-profile `pactl list short | grep bluez_card | awk '{print $1}'` a2dp_sink
+fi 
