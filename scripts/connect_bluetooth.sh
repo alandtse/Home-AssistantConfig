b7696e722e46e17c8b22f3ff451f7996afff063d
Alan Tse <alandtse@gmail.com>
2018-01-02 18:40:14 -0800

    Updating bluetooth connect and tts components

--- scripts/connect_bluetooth.sh
+++ scripts/connect_bluetooth.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-test=`pactl list short | grep bluez_sink`
-if [[ -z $test ]]; then
-  /home/homeassistant/.homeassistant/scripts/bluetooth.sh 84:D6:D0:21:85:21
-  sleep 4
-  pactl set-card-profile `pactl list short | grep bluez_card | awk '{print $1}'` a2dp_sink
+test="pactl list short | grep bluez_sink"
+controllers=(B8:27:EB:00:B4:BF 00:1B:DC:0F:D2:E1)
+speakers=(84:D6:D0:21:85:21 00:FC:8B:D5:99:F3)
+
+for i in ${!speakers[@]}; do
+speaker=${speakers[i]}
+#echo $speaker
+command="$test.${speaker//:/_}" 
+#echo $command
+result=`eval $command`
+#echo $result
+if [[ -z $result ]]; then
+  echo "Connecting to $speaker"
+  /home/homeassistant/.homeassistant/scripts/bluetooth.sh ${speakers[i]} ${controllers[i]}
+  #pactl set-card-profile `pactl list short | grep bluez_card | awk '{print $1}'` a2dp_sink
+else
+  echo "Already connected to $speaker"
 fi 
+done
